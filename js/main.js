9b89779b2451721bb4043f4bfb806b121f8321a9
Romy <mail@romy.dev>
2024-01-26 11:35:07 +0100

    x & h1

--- js/main.js
+++ js/main.js
@@ -1,10 +1,18 @@
 const button = document.getElementById("js--menu");
 const navigation = document.getElementById("js--nav");
 const body = document.getElementById("js--body");
+const xButton = document.getElementById("js--xbutton");
 
 button.onclick = function()
 {
     navigation.style.visibility = "visible";
     navigation.style.opacity = 1;
     body.style.overflow = "hidden";
+}
+
+xButton.onclick = function()
+{
+    navigation.style.visibility = "hidden";
+    navigation.style.opacity = 0;
+    body.style.overflow = "visible";
 }
